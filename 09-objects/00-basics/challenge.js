1af05cbb5478e66f6e56474b106fbaf4b90d73f8
Samantha Healy <samhealy09@gmail.com>
2015-02-24 15:30:55 -0500

    all tests passing

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,29 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  };
+  return course;
+}
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  if (object[newProp] === undefined) {
+    object[newProp] = newValue;
+  }
+  return object;
+}
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+}
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  var items = {
+    "Mac Pro": 2499,
+    "MacBook Pro": 1299,
+    "MacBook Air": 999,
+    "Apple Sticker": 1
+  };
+  return items[item] <= money;
+}

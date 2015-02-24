f318ea212ae471fb421467dd03e68b0020fd7e8b
Samantha Healy <samhealy09@gmail.com>
2015-02-24 09:34:22 -0500

    tests passing

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,9 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(one, two, three, four) {
+  var newArray = [one, two, three, four];
+  return newArray;
+}
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(array) {
+  var lastElementIndex = array.length - 1;
+  return [array[0], array[lastElementIndex]];
+}

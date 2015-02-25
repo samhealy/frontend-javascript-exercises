b368f9374c28e408b4f3420cbbed97f094f3fd79
Samantha Healy <samhealy09@gmail.com>
2015-02-25 13:38:08 -0500

    tests passing

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,15 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn) {
+	while (conditionalFn()) {
+		actionFn();
+	}
+}
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
+	sum = 0;
+	i = 0;
+	while (i < array.length) {
+		sum = array[i] + sum;
+	}
+	return sum;
+}
 

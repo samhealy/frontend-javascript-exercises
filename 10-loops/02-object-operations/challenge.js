12574b3cb8619de8f32fb1f56a8b259e029da9b6
Samantha Healy <samhealy09@gmail.com>
2015-02-25 13:02:36 -0500

    tests passing

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,29 @@
-module.exports.copy = undefined;
+module.exports.copy = function(object) {
+	var newObject = {};
+	for (var key in object) {
+		newObject[key] = object[key];
+	}
+	return newObject;
+}
 
-module.exports.extend = undefined;
-
-module.exports.hasElems = undefined;
+module.exports.extend = function(dest, src) {
+	for (var key in src) {
+    	dest[key] = src[key];
+  	}
+  	return dest;
+}
+// object = { name: "Computer", cost: "$1,000" };
+module.exports.hasElems = function(object, elementArray) {
+	counter = 0
+	for (var position in elementArray) {
+		if (object.hasOwnProperty(elementArray[position])){
+			counter++
+		}
+	}
+	if (counter >= elementArray.length) {
+		return true
+	}
+	else {
+		return false
+	}
+}

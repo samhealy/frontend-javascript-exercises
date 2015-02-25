cabf6a21d0560f9a08fd86f5668dca7698b02f30
Samantha Healy <samhealy09@gmail.com>
2015-02-25 11:00:55 -0500

    tests passing

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,20 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
+	var sum = 0;
+	for (var counter = 0; counter < array.length; counter++) {
+		sum += array[counter];
+	}
+	return sum;
+}
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+	var outputString = inputString.toLowerCase().split(",");
+	return outputString;
+}
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(array) {
+	var output = [];
+	for (var i = 0; i < array.length; i++) {
+		output.push(i + " is " + array[i]);
+	}
+	return output;
+}

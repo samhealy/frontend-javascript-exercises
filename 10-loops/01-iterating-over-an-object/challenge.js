519babf89d13fe2920a804a19b8fb44f8203fac5
Samantha Healy <samhealy09@gmail.com>
2015-02-25 11:29:32 -0500

    all tests passing

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,23 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(object) {
+	var array = [];
+	for (var key in object) {
+		array.push(key);
+	}
+	return array;
+}
 
-module.exports.getValues = undefined;
+module.exports.getValues = function(object) {
+	var array = [];
+	for (var value in object) {
+		array.push(object[value]);
+	}
+	return array; 
+}
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(object) {
+	var array = [];
+	for (var key in object) {
+		array.push(key + " is " + object[key]);
+	}
+	return array;
+}
